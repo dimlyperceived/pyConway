a2cbddee6c4e89752d97e7d523870cd2a19c5670
Daniel Everson <daniel@connectedsignals.com>
2015-03-15 23:05:47 -0700

    Changed virtualenv location.

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/daniel/Google Drive/development/conwaylife/env_life"
+set -gx VIRTUAL_ENV "/Users/daniel/Google Drive/development/conwaylife/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
