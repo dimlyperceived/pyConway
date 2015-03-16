d2038103787f77e429ac4445a0d06e7810447362
Daniel Everson <daniel@connectedsignals.com>
2015-03-15 23:06:31 -0700

    Moved most logic into the Grid class - don't actually like this, but it's currently a minimally functional example.

--- mylife.py
+++ mylife.py
@@ -16,53 +16,56 @@ class Grid(object):
             str += ''.join(i)
         return str
 
+    def neighbor_count(self, x, y):
+        n_ = grid.query(x, y+1)
+        ne = grid.query(x+1, y+1)
+        e_ = grid.query(x+1, y)
+        se = grid.query(x+1, y-1)
+        s_ = grid.query(x, y-1)
+        sw = grid.query(x-1, y-1)
+        w_ = grid.query(x-1, y)
+        nw = grid.query(x-1, y+1)
+        neighbors = [n_, ne, e_, se, s_, sw, w_, nw]
+        
+        count = 0
+        for n in neighbors:
+            if n == ALIVE:
+                count += 1
+        return count
+
+    def compute_state(self, ncount, currstate):
+        if currstate == ALIVE:
+            if ncount < 2:
+                return EMPTY
+            elif ncount > 3:
+                return EMPTY
+        else:
+            if ncount == 3:
+                return ALIVE
+        return currstate
+
     def query(self, x, y):
         return self.rows[y % self.h][x % self.w]
 
     def assign(self, x, y, state):
         self.rows[y % self.h][x % self.w] = state
 
-def get_neighbor_count(grid, x, y):
-    n_ = grid.query(x, y+1)
-    ne = grid.query(x+1, y+1)
-    e_ = grid.query(x+1, y)
-    se = grid.query(x+1, y-1)
-    s_ = grid.query(x, y-1)
-    sw = grid.query(x-1, y-1)
-    w_ = grid.query(x-1, y)
-    nw = grid.query(x-1, y+1)
-
-    neighbors = [n_, ne, e_, se, s_, sw, w_, nw]
-
-    count = 0
-    for n in neighbors:
-        if n == ALIVE:
-            count += 1
-            
-    return count
-
-def compute_state(ncount, currstate):
-    if currstate == ALIVE:
-        if ncount < 2:
-            return EMPTY
-        elif ncount > 3:
-            return EMPTY
-    else:
-        if ncount == 3:
-            return ALIVE
-    return currstate
-
-def step_grid(grid):
-    for x in range(grid.w):
+    def step(self):
+        assign_list = []
         for y in range(grid.h):
-            ncount = get_neighbor_count(grid, x,y)
-            grid.assign(x, y, compute_state(ncount, grid.query(x, y)))
+            for x in range(grid.w):
+                ncount = self.neighbor_count(x,y)
+                new_state = self.compute_state(ncount, self.query(x,y))
+                if new_state != self.query(x,y):
+                    assign_list.append((x,y,new_state))
+        for t in assign_list:
+            self.assign(t[0], t[1], t[2])
 
 
 def print_ncount(grid):
     for y in range(grid.h):
         for x in range(grid.w):
-            ncount = get_neighbor_count(grid, x, y)
+            ncount = grid.neighbor_count(x, y)
             print(ncount, grid.query(x, y), end="", sep="")
         print()
 
@@ -77,8 +80,7 @@ if __name__ == "__main__":
     grid.assign(4, 3, ALIVE)
 
     print(grid)
-    print_ncount(grid)
 
-    # for i in range(10):
-    #     step_grid(grid)
-    #     print(grid)
+    for i in range(10):
+        grid.step()
+        print(grid)
